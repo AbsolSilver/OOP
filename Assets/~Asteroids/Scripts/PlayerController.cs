8f45f9395601cd324674ba186c4c455f8fd4302b
anthony.demovic1 <anthony.demovic1@600900B211DE413>
2018-02-28 11:57:09 +1100

    Restarted Asteroids project

---
+++ Assets/~Asteroids/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Moving movement;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            float inputH = Input.GetAxis("Horizontal");
+            // Check if up arrow is pressed
+            if (inputV > 0)
+            {
+                // Accelerate player
+                movement.Accelerate(transform.up);
+            }
+
+            // Check if horizontal axis (left or right arrow) is pressed
+            // Rotate in correct direction
+            movement.Rotate(inputH);
+        }
+    }
+}
+
