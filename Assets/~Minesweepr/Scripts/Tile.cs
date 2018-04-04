d6c005b184898ccf83136e3157efcfede8cebaba
anthony.demovic1 <anthony.demovic1@600900B211DE413>
2018-04-04 12:00:29 +1000

    Made Minesweeper2D

---
+++ Assets/~Minesweepr/Scripts/Tile.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        // Store x and y coordinate in array for later use
+        public int x = 0;
+        public int y = 0;
+        public bool isMine = false;
+        public bool isRevealed = false;
+        [Header("References")]
+        public Sprite[] emptySprites;
+        public Sprite[] mineSprites;
+
+        private SpriteRenderer rend;
+
+        void Awake()
+        {
+            rend = GetComponent<SpriteRenderer>();
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            isMine = Random.value < .05f;
+        }
+
+        public void Reveal (int adjacentMines, int mineState = 0)
+        {
+            isRevealed = true;
+
+            if(isMine)
+            {
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}
