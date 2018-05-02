2a3279108b35a834ebddfa7c309d11098e8ec154
anthony.demovic1 <anthony.demovic1@600900B211DE407>
2018-05-02 11:47:19 +1000

    Made stuff

---
+++ unity/Assets/~Asteroids/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Moving movement;
+        public Shooting shoot;
+
+        #region Unity Functions
+
+        // Update is called once per frame
+        void Update()
+        {
+            Shoot();
+            Movement();
+        }
+
+        #endregion
+
+        #region Custom Functions
+
+        // Handling Shooting Functionality
+        void Shoot()
+        {
+            //If Space is pressed
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                // Fire!
+                shoot.Fire(transform.up);
+            }
+
+        }
+
+
+        // Handles Moving Functionality
+        void Movement()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            float inputH = Input.GetAxis("Horizontal");
+            // Check if up arrow is pressed
+            if (inputV > 0)
+            {
+                // Accelerate player
+                movement.Accelerate(transform.up);
+            }
+
+            // Rotate in correct direction
+            movement.Rotate(inputH);
+        }
+
+        #endregion
+    }
+}
+
---
+++ visualstudio/ClassesAndObjects/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAndObjects
+{
+    class Colour
+    {
+        public float r, g, b;
+    }
+
+    class Dog
+    {
+        public string name;
+        public int size;
+        public string breed;
+        public ConsoleColor colour;
+
+        public void Walk()
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(name + " is walking.");
+            Console.ReadLine();
+        }
+
+        public void Eat(string food)
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(name + " is eating" + food);
+            Console.ReadLine();
+        }
+
+        public void Shit()
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(name + " is shitting");
+            Console.ReadLine();
+        }
+
+        public void Sleep()
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(name + " is sleeping");
+            Console.ReadLine();
+        }
+
+        public void Wag()
+        {
+            Console.WriteLine(name + " is wagging its tail");
+            Console.ReadLine();
+        }
+
+        public void Speak()
+        {
+            Console.ForegroundColor = colour;
+            Console.WriteLine(name + " says: Woof!");
+            Console.ReadLine();
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dog 1
+            Dog dog1 = new Dog();
+            dog1.name = "Fido";
+            dog1.size = 5;
+            dog1.breed = "Cavoodle";
+            dog1.colour = ConsoleColor.Green;
+
+            Dog dog2 = new Dog();
+            dog2.name = "Rover";
+            dog2.size = 10;
+            dog2.breed = "Great Dane";
+            dog2.colour = ConsoleColor.Yellow;
+
+            Dog dog3 = new Dog();
+            dog3.name = "Spots";
+            dog3.size = 7;
+            dog3.breed = "Dalmation";
+            dog3.colour = ConsoleColor.White;
+
+            dog1.Speak();
+            dog2.Speak();
+            dog3.Speak();
+            dog1.Walk();
+            dog1.Eat(" Dog Food");
+            dog1.Shit();
+            Colour black = new Colour();
+            #endregion
+        }
+    }
+}
