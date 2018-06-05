9e1960eb10a42f77120c35c97838a2a751209200
AbsolSilver <AbsolSilver@DESKTOP-0L2U9GV>
2018-06-05 16:09:05 +1000

    Redid OOP (Breakout)

--- unity/Assets/~Breakout/Scripts/Paddle.cs
+++ unity/Assets/~Breakout/Scripts/Paddle.cs
@@ -8,30 +8,24 @@ namespace Breakout
     {
         public float movementSpeed = 20f;
         public Ball currentBall;
-        // Directions array defaults to two values
         public Vector2[] directions = new Vector2[]
         {
-            new Vector2(-0.5f, 0.5f),
+            new Vector2 (-0.5f, 0.5f),
             new Vector2(0.5f, 0.5f)
         };
 
         // Use this for initialization
         void Start()
         {
-            // Grabs currentBall from children of the Paddle
             currentBall = GetComponentInChildren<Ball>();
         }
 
         void Fire()
         {
-            // Detach as child
             currentBall.transform.SetParent(null);
-            // Generate random dir from list of directions
             Vector3 randomDir = directions[Random.Range(0, directions.Length)];
-            // Fire off ball in randomDirection
             currentBall.Fire(randomDir);
         }
-
 
         void CheckInput()
         {
@@ -43,15 +37,10 @@ namespace Breakout
 
         void Movement()
         {
-            // Get input on the horizontal axis
             float inputH = Input.GetAxis("Horizontal");
-            // Set force to direction (inputH to decide which direction)
             Vector3 force = transform.right * inputH;
-            // Apply movement speed to force
             force *= movementSpeed;
-            // Apply delta time to force
             force *= Time.deltaTime;
-            // Add force to position
             transform.position += force;
         }
 
